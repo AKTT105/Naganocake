0af5a5c7022eb1a3db854e21f76d8b7f23b5e40d
EC2 Default User <ec2-user@ip-172-31-55-105.ec2.internal>
2020-12-02 06:37:38 +0000

    12/2分

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,6 @@
 class Genre < ApplicationRecord
-    belongs_to :products
-    
+    belongs_to :products, optional: true
+
     validates :name, presence: true
-    validates :is_active, inclusion: { in: [true, false] }
+    #validates :is_active, inclusion: { in: [true, false] }
 end
