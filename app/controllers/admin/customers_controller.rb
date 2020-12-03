cf098a30a1c2e484f0969da510579c79e9dfb846
EC2 Default User <ec2-user@ip-172-31-55-105.ec2.internal>
2020-12-03 08:05:33 +0000

    妻夫木修正箇所

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,19 +1,38 @@
 class Admin::CustomersController < ApplicationController
-    
-　def index
-　  @customer = Customer.all
-　end
-　
-　def show
-　  @customer = Customer.find(params[:id])
-　end
-　
-　def edit
-    @customer = Customer.find(params[:id])
-　end
-　
-　private
-　def customer_params
-　  params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :phone_number, :email, :is_deleted)
-　end
+
+ def index
+   @customer = Customer.all
+ end
+
+# 　def create
+#     @customer = Customer.find(params[:id])
+#     if @customer.update(customer_params)
+#       flash[:notice] = "Signed in successfully."
+#       redirect_to admin_customer_path(@customer)
+#     else
+#       flash[:notice] = "error"
+#       render :new
+#     end
+#   end
+
+def destroy
+   @customer = Customer.find(params[:id])
+    if @customer.destroy
+     flash[:notice] = "Signed out successfully."
+     redirect_to new_admin_session_path
+    end
+end
+
+ def show
+  @customer = Customer.find(params[:id])
+ end
+
+ def edit
+  @customer = Customer.find(params[:id])
+ end
+
+ private
+  def customer_params
+   params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :phone_number, :email, :is_deleted)
+  end
 end
