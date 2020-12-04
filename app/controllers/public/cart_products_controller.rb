7265f6b61a560a8c88fc54b92e2120a9cdcbecce
EC2 Default User <ec2-user@ip-172-31-8-23.ec2.internal>
2020-12-04 08:45:55 +0000

    高橋さんとマージ２

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -1,6 +1,29 @@
 class Public::CartProductsController < ApplicationController
     
     def index
-      @product = Product.find(params[:id])
+      @cart_products = CartProduct.where(product_id: Product.ids)
+    end
+    
+    def create
+      @product = Product.find_by(params[:product_id])
+      @cart_product = CartProduct.new(cart_product_params)
+      @cart_product.product_id = @product.id
+      @cart_product.customer_id = current_customer
+      @cart_product.save
+      redirect_to cart_products_path
+      logger.debug @cart_product.errors.inspect
+    end
+    
+    def destroy
+      @cart_product = CartProduct.find(params[:id])
+      if @cart_product.destroy
+      flash[:notice] = ""
+      redirect_to cart_product_path
+      end
+    end
+    
+    private
+    def cart_product_params
+      params.require(:cart_product).permit(:amount)
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :genre
   has_many :order_products, dependent: :destroy
+  has_many :cart_products, dependent: :destroy
 
   attachment :image
   validates :name, presence: true
