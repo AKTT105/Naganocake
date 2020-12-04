303b2325da66de78cbe020b684a02096fafa252f
EC2 Default User <ec2-user@ip-172-31-55-105.ec2.internal>
2020-12-04 06:03:58 +0000

    AdminViewモデル削除

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,14 +1,14 @@
 class Order < ApplicationRecord
-  
+
   belongs_to :customer
-  
+
   has_many :order_products, dependent: :destroy
-  
+
   enum payment_type: {
     クレジットカード: 0,
     銀行振込:         1
   }
-  
+
   enum status: {
     入金待ち:   0,
     入金確認:   1,
