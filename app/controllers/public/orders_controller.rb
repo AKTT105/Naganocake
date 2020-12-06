49bf4be1db3bba6349969d85559c949395fd1984
EC2 Default User <ec2-user@ip-172-31-55-105.ec2.internal>
2020-12-06 04:02:59 +0000

    小宮山さんマージ前

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,25 +1,33 @@
 class Public::OrdersController < ApplicationController
-  
+
   def index
-    @orders = Order.page(params[:page]).reverse_order
+    @orders = Order.where(customer_id: current_customer)
+    #binding.pry
   end
-  
+
   def show
     @order = Order.find(params[:id])
     @order_products = OrderProduct.where(order_id: @order)
     @customer = Customer.find_by(id: @order.customer_id)
   end
-  
+
   def new
+    @customer = Customer.find_by(id: @order.customer_id)
+    @customer_address = Customer.find_by(id: @order.customer_id)
   end
-  
+
   def confirm
   end
-  
+
   def create
   end
-  
+
   def done
   end
-  
+
+  private
+  def order_params
+    params.require(:order).permit(:id,:customer_id,:postal_code,:address,:name,:total_payment,:payment_type,:status)
+  end
+
 end
