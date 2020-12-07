1b3270c49cb5eb3bc0a8ee429e9dd113ca7a947a
EC2 Default User <ec2-user@ip-172-31-8-23.ec2.internal>
2020-12-07 02:12:19 +0000

    12/7に１回目のマージ

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -2,24 +2,39 @@ class Public::CartProductsController < ApplicationController
     
     def index
       @cart_products = CartProduct.where(product_id: Product.ids)
+      #@cart_product = CartProduct.find(params[:id])
+      #@total_price = @cart_products.sum
     end
     
     def create
-      @product = Product.find_by(params[:product_id])
-      @cart_product = CartProduct.new(cart_product_params)
-      @cart_product.product_id = @product.id
-      @cart_product.customer_id = current_customer
-      @cart_product.save
+      cart_product = CartProduct.new(cart_product_params)
+      product = Product.find(params[:product_id])
+      cart_product.product_id = product.id
+      customer = Customer.find_by(id: current_customer)
+      #binding.pry
+      cart_product.customer_id = customer.id
+      cart_product.save
       redirect_to cart_products_path
-      logger.debug @cart_product.errors.inspect
+    end
+    
+    def update
+      @cart_product = CartProduct.find(params[:id])
+      @cart_product.update(cart_product_params)
+      redirect_to cart_products_path
     end
     
     def destroy
       @cart_product = CartProduct.find(params[:id])
-      if @cart_product.destroy
-      flash[:notice] = ""
-      redirect_to cart_product_path
-      end
+      #binding.pry
+      @cart_product.destroy
+      redirect_to cart_products_path
+    end
+    
+    def destroy_all
+      CartProduct.destroy_all
+      #@cart_products = CartProduct.where(product_id: Product.ids)
+      #@cart_products.destroy_all
+      redirect_to cart_products_path
     end
     
     private
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,17 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :cart_products, dependent: :destroy
+  has_many :orders, dependent: :destroy
+  has_many :deliveries, dependent: :destroy
+  
+def cart_total_price
+  total = 0
+  cart_products.each do |cart_product|
+    total += cart_product.tax_on_price
+  end
+  total
+end
+
 end
