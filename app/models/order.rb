f9255fcc0bbdc79cd8583e5988cfa1074cc1fb7f
EC2 Default User <ec2-user@ip-172-31-55-105.ec2.internal>
2020-12-07 02:12:04 +0000

    妻夫木編集部分(パブリック注文関係)

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,10 +3,11 @@ class Order < ApplicationRecord
   belongs_to :customer
 
   has_many :order_products, dependent: :destroy
+  attachment :product
 
   enum payment_type: {
-    クレジットカード: 0,
-    銀行振込:         1
+    クレジットカード: 1,
+    銀行振込:         2
   }
 
   enum status: {
@@ -16,6 +17,13 @@ class Order < ApplicationRecord
     発送準備中: 3,
     発送済み:   4
   }
+
+  enum address_type: {
+    ご自身の住所: :A,
+    登録済み住所: :B,
+    新しいお届け先: :C,
+  }
+
 
   def order_total_price
     order_products.to_a.sum{|order_product| order_product.total_price}
