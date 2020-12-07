e24c2e8e999660107cb021fc8f9d8c4813d26a40
EC2 Default User <ec2-user@ip-172-31-71-233.ec2.internal>
2020-12-07 00:52:59 +0000

    first-commit

---
+++ config/routes.rb
@@ -0,0 +1,34 @@
+Rails.application.routes.draw do
+
+  devise_for :customers
+  scope module: :public do
+    root 'homes#top'
+    resources :products, only: [:index, :show] do
+      collection do
+        get 'search'
+      end
+    end
+    resources :cart_products, only: [:index, :update, :destroy, :create]
+      delete 'cart_products/destroy_all' => 'cart_products#destroy_all'
+    resources :orders, only: [:new, :create, :index, :show]
+      post 'orders/confirm' => 'orders#confirm'
+      get 'orders/done' => 'orders#done'
+    resource :customers, only: [:edit, :update]
+      get 'customers/my_page' => 'customers#show'
+      get 'customers/unsubscribe' => 'customers#unsubscribe'
+      patch 'customers/withdraw' => 'customers#withdraw'
+    resources :deliveries, only: [:index, :create, :destroy, :edit, :update]
+  end
+
+  devise_for :admin
+  namespace :admin do
+    get '' => 'homes#top'
+    resources :products, only: [:index, :new, :create, :show, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:index, :show, :update]
+    resources :order_products, only: [:update]
+    resources :genres, only: [:index, :create, :edit, :update]
+  end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
