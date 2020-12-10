12c5f398b745b8b9c5d6d43c08a95aeb814e2768
EC2 Default User <ec2-user@ip-172-31-71-233.ec2.internal>
2020-12-10 02:11:22 +0000

    [Update]admin/order&public/product

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,15 @@
 class Admin::OrdersController < ApplicationController
     
     def index
-      @orders = Order.page(params[:page]).reverse_order
+      path = Rails.application.routes.recognize_path(request.referer)
+      if path[:controller] == "admin/homes" && path[:action] == "top"
+        @orders = Order.where(created_at: Date.today.all_day).page(params[:page]).reverse_order
+      elsif path[:controller] == "admin/customers" && path[:action] == "show"
+        customer = Customer.find(params[:customer_id])
+        @orders = customer.orders.page(params[:page]).reverse_order
+      else 
+        @orders = Order.page(params[:page]).reverse_order
+      end
     end
     
     def show
@@ -12,7 +20,14 @@ class Admin::OrdersController < ApplicationController
     
     def update
       order = Order.find(params[:id])
+      order_products = OrderProduct.where(order_id: order.id)
       order.update(order_params)
+      if order.status == "入金確認"
+        order_products.each do |op|
+          op.update(making_status: "製作待ち")
+        end
+      end
+      
       redirect_to admin_order_path(order)
     end
     
