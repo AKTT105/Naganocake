f38b3d1da54f4eb38e69cdf92ece685f1b469c5c
EC2 Default User <knk61721@gmail.com>
2020-12-13 10:19:23 +0000

    配送情報編集画面変更

--- app/controllers/public/deliveries_controller.rb
+++ app/controllers/public/deliveries_controller.rb
@@ -1,11 +1,11 @@
 class Public::DeliveriesController < ApplicationController
   before_action :authenticate_customer!
-    
+
   def index
     @deliveries = Delivery.where(customer_id: current_customer)
     @delivery = Delivery.new
   end
-  
+
   def create
     @delivery = Delivery.new(delivery_params)
     @customer = Customer.find_by(id: current_customer)
@@ -19,15 +19,15 @@ class Public::DeliveriesController < ApplicationController
       render :index
     end
   end
-  
+
   def edit
     @delivery = Delivery.find(params[:id])
   end
-  
+
   def update
     #@delivery = Delivery.find(params[:id])
-    @deliveries = Delivery.where(customer_id: current_customer)
-    if @deliveries.update(delivery_params)
+    @delivery = Delivery.find(params[:id])
+    if @delivery.update(delivery_params)
       flash[:notice] = ""
       redirect_to deliveries_path
     else
@@ -35,13 +35,13 @@ class Public::DeliveriesController < ApplicationController
       render :index
     end
   end
-  
+
   def destroy
       @delivery = Delivery.find(params[:id])
       @delivery.destroy
       redirect_to deliveries_path
   end
-  
+
  private
   def delivery_params
       params.require(:delivery).permit(:name, :postal_code, :address, :customer_id)
