de0d9ebc5d636beb2cc13ef6b69f467854064f5e
EC2 Default User <knk61721@gmail.com>
2020-12-13 02:19:50 +0000

    最終マージ

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,18 +1,21 @@
 class Admin::SearchesController < ApplicationController
-  
+  before_action :authenticate_admin!
+
   def search
     @model = params[:model]
-    keyward = params[:keyward]
+    @keyward = params[:keyward]
     if @model == 'customer'
-      if keyward.blank?
+      if @keyward.blank?
         @title = '会員'
         @customers = Customer.page(params[:page]).per(10).reverse_order
       else
         @title = '会員'
-        @customers = Customer.where("first_name LIKE?", "%#{keyward}%").page(params[:page]).per(10).reverse_order
+        @keyward.split(/[[:blank:]]+/).each do |keyward|
+          @customers = Customer.where("first_name LIKE? OR last_name LIKE?", "%#{keyward}%", "%#{keyward}%").page(params[:page]).per(10).reverse_order
+        end
       end
     elsif @model == 'product'
-      if keyward.blank?
+      if @keyward.blank?
         @title = '商品'
         @products = Product.page(params[:page]).per(10).reverse_order
       else
@@ -21,5 +24,5 @@ class Admin::SearchesController < ApplicationController
       end
     end
   end
-  
+
 end
